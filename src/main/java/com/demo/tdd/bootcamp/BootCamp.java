677a2685abd64717e0664f3e5ec20bccf610a1a2
ZhaoZhang <zhaozhang@thoughtworks.com>
2018-08-21 21:22:49 +0800

    refactor

--- src/main/java/com/demo/tdd/bootcamp/BootCamp.java
+++ src/main/java/com/demo/tdd/bootcamp/BootCamp.java
@@ -16,14 +16,11 @@ public class BootCamp {
   }
 
   public String countOff(int count) {
+    StringBuilder result = new StringBuilder();
     for (int i = 0; i < specials.size(); i++) {
       if (String.valueOf(count).contains(String.valueOf(specials.get(0)))) {
         return specialFlag[i];
       }
-    }
-
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < specials.size(); i++) {
       if (count % specials.get(i) == 0) {
         result.append(specialFlag[i]);
       }
