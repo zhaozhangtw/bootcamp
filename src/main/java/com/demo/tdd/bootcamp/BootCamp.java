5f2dedb8e98c12538b741e50aef3635a95cb7cfc
ZhaoZhang <zhaozhang@thoughtworks.com>
2018-08-21 20:50:14 +0800

    count off times of one special number

--- src/main/java/com/demo/tdd/bootcamp/BootCamp.java
+++ src/main/java/com/demo/tdd/bootcamp/BootCamp.java
@@ -3,14 +3,24 @@ package com.demo.tdd.bootcamp;
 public class BootCamp {
 
   private final int firstSpecial;
+  private final int secondSpecial;
+  private final int thirdSpecial;
 
-  public BootCamp(int firstSpecial) {
+  public BootCamp(int firstSpecial, int secondSpecial, int thirdSpecial) {
     this.firstSpecial = firstSpecial;
+    this.secondSpecial = secondSpecial;
+    this.thirdSpecial = thirdSpecial;
   }
 
   public String countOff(int count) {
     if (count % firstSpecial == 0) {
       return "Fizz";
+    }
+    if (count % secondSpecial == 0) {
+      return "Buzz";
+    }
+    if (count % thirdSpecial == 0) {
+      return "Whizz";
     }
 
     return count + "";
--- src/test/java/com/demo/tdd/bootcamp/BootCampTest.java
+++ src/test/java/com/demo/tdd/bootcamp/BootCampTest.java
@@ -8,16 +8,24 @@ public class BootCampTest {
 
   @Test
   public void testCountOffNormalNumber() {
-    BootCamp bootCamp = new BootCamp(3);
+    BootCamp bootCamp = new BootCamp(3, 5, 7);
 
     assertEquals("2", bootCamp.countOff(2));
   }
 
   @Test
   public void testCountOffTimesOfFirstSpecialNumber() {
-    BootCamp bootCamp = new BootCamp(3);
+    BootCamp bootCamp = new BootCamp(3, 5, 7);
 
     assertEquals("Fizz", bootCamp.countOff(3));
     assertEquals("Fizz", bootCamp.countOff(6));
+  }
+
+  @Test
+  public void testCountOffTimesOfOneSpecialNumber() {
+    BootCamp bootCamp = new BootCamp(3, 5, 7);
+
+    assertEquals("Buzz", bootCamp.countOff(25));
+    assertEquals("Whizz", bootCamp.countOff(28));
   }
 }
