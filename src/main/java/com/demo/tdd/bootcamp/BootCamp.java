14fa526bfb3260dc5806fa23b6492099c6cf8e63
ZhaoZhang <zhaozhang@thoughtworks.com>
2018-08-21 20:43:06 +0800

    count off times of first special number and pseudo realization

--- src/main/java/com/demo/tdd/bootcamp/BootCamp.java
+++ src/main/java/com/demo/tdd/bootcamp/BootCamp.java
@@ -2,11 +2,15 @@ package com.demo.tdd.bootcamp;
 
 public class BootCamp {
 
-  public BootCamp() {
+  public BootCamp(int firstSpecial) {
 
   }
 
   public String countOff(int count) {
+    if (count % 3 == 0) {
+      return "Fizz";
+    }
+
     return count + "";
   }
 }
--- src/test/java/com/demo/tdd/bootcamp/BootCampTest.java
+++ src/test/java/com/demo/tdd/bootcamp/BootCampTest.java
@@ -8,8 +8,16 @@ public class BootCampTest {
 
   @Test
   public void testCountOffNormalNumber() {
-    BootCamp bootCamp = new BootCamp();
+    BootCamp bootCamp = new BootCamp(3);
 
     assertEquals("2", bootCamp.countOff(2));
+  }
+
+  @Test
+  public void testCountOffTimesOfFirstSpecialNumber() {
+    BootCamp bootCamp = new BootCamp(3);
+
+    assertEquals("Fizz", bootCamp.countOff(3));
+    assertEquals("Fizz", bootCamp.countOff(6));
   }
 }
